e5d6296f526c573d707dbf08968fc9c1c211d367
saf <slawek.rudnicki@gmail.com>
2017-05-16 23:55:37 +0200

    Add word-transcription tests; change voicing model
    To handle the phenomenon of forward devoicing of the [V] phoneme in
    Polish, we change the model for changing voicing from one based on
    individual letters to a new one which does two passes: one forward and
    one backwards.
    
    Lots of transcription unit tests are failing because the backward pass
    is not implemented yet.

--- src/pronunciation/pl/transcription.rs
+++ src/pronunciation/pl/transcription.rs
@@ -29,14 +29,14 @@ fn devoices(p: &phoneset::Phone) -> bool {
     modifies_voicing(p) && match p.features() {
         Consonant { phonation: Unvoiced, .. } => true,
         _ => false,
-    }   
+    }
 }
 
 fn envoices(p: &phoneset::Phone) -> bool {
     modifies_voicing(p) && match p.features() {
         Consonant { phonation: Voiced, manner: ref m, .. } if *m != Nasal => true,
         _ => false,
-    }   
+    }
 }
 
 fn transcribe_char(prev_chars: &[char],
@@ -45,8 +45,6 @@ fn transcribe_char(prev_chars: &[char],
     assert!(chars.len() > 0);
     let (this_char, next_chars) = chars.split_first().unwrap();
     let next_phone = next_phones.first();
-    let devoiced = match next_phone { None => false, Some(ref p) => devoices(p) };
-    let envoiced = match next_phone { None => false, Some(ref p) => envoices(p) };
 
     let no_char = '#';
     let prev_char = prev_chars.last().unwrap_or(&no_char);
@@ -66,26 +64,26 @@ fn transcribe_char(prev_chars: &[char],
                     _ => tr![O Wx],
                 }
             },
-        'b' => if devoiced { tr![P] } else { tr![B] },
+        'b' => tr![B],
         'c' =>
             match *next_char {
-                'z' => if envoiced { tr![Dzh] } else { tr![Cz] },
+                'z' => tr![Cz],
                 'i' => tr![Ci],
                 'h' => tr![H],
-                _ => if envoiced { tr![Dz] } else { tr![C] },
+                _ => tr![C],
             },
-        'ć' => if envoiced { tr![Dzi] } else { tr![Ci] },
+        'ć' => tr![Ci],
         'd' =>
             match *next_char {
                 'z' =>
                     if *nnext_char == 'i' {
                         tr![Dzi]
-                    } else {                      
-                        if devoiced { tr![C] } else { tr![Dz] }
+                    } else {
+                        tr![Dz]
                     },
-                'ź' => if devoiced { tr![Ci] } else { tr![Dzi] },
-                'ż' => if devoiced { tr![Cz] } else { tr![Dzh] },
-                _ => if devoiced { tr![T] } else { tr![D] },
+                'ź' => tr![Dzi],
+                'ż' => tr![Dzh],
+                _ => tr![D],
             },
         'e' => tr![E],
         'ę' =>
@@ -99,8 +97,8 @@ fn transcribe_char(prev_chars: &[char],
                     _ => tr![E Wx],
                 }
             },
-        'f' => if envoiced { tr![V] } else { tr![F] },
-        'g' => if devoiced { tr![K] } else { tr![G] },
+        'f' => tr![F],
+        'g' => tr![G],
         'h' => if *prev_char == 'c' { tr![] } else { tr![H] },
         'i' =>
             if let Some(p) = next_phone {
@@ -116,7 +114,7 @@ fn transcribe_char(prev_chars: &[char],
                 tr![I]
             },
         'j' => tr![J],
-        'k' => if envoiced { tr![G] } else { tr![K] },
+        'k' => tr![K],
         'l' => tr![L],
         'ł' => tr![W],
         'm' => tr![M],
@@ -135,41 +133,72 @@ fn transcribe_char(prev_chars: &[char],
         'ń' => tr![Ni],
         'o' => tr![O],
         'ó' => tr![U],
-        'p' => if envoiced { tr![B] } else { tr![P] },
+        'p' => tr![P],
         'r' =>
             if *next_char == 'z' {
-                if devoiced { tr![Sz] } else { tr![Zh] }
+                tr![Zh]
             } else {
                 tr![R]
             },
         's' =>
             match *next_char {
-                'i' => if envoiced { tr![Zi] } else { tr![Si] },
-                'z' => if envoiced { tr![Zh] } else { tr![Sz] },
-                _   => if envoiced { tr![Z]  } else { tr![S]  },
+                'i' => tr![Si],
+                'z' => tr![Sz],
+                _   => tr![S],
             },
-        'ś' => if envoiced { tr![Zi] } else { tr![Si] },
-        't' => if envoiced { tr![D] } else { tr![T] },
+        'ś' => tr![Si],
+        't' => tr![T],
         'u' => tr![U],
-        'w' => if devoiced { tr![F] } else { tr![V] },
+        'w' => tr![V],
         'y' => tr![Y],
         'z' =>
             match *prev_char {
                 'c' | 'd' | 'r' | 's' => tr![],
-                _ => if devoiced { tr![S] } else { tr![Z] },
+                _ => tr![Z],
             },
         'ź' =>
             match *prev_char {
                 'd' => tr![],
-                _ => if devoiced { tr![Si] } else { tr![Zi] },
+                _ => tr![Zi],
             },
         'ż' =>
             match *prev_char {
                 'd' => tr![],
-                _ => if devoiced { tr![Sz] } else { tr![Zh] },
+                _ => tr![Zh],
             },
         _   => None
     }
+}
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum VoicingState {
+    /// Retain the natural phonation of the phones.
+    Neutral,
+
+    /// Envoice phones which can be envoiced.
+    Voice,
+
+    /// Devoice phones which can be devoiced.
+    Devoice,
+}
+
+/// Forward pass of phonation changes.
+///
+/// The only thing that happens here in Polish is a devoicing of [V] after
+/// unvoiced consonants.
+///
+/// This function is suitable for `scan` on an iterator over the phones.
+fn change_voicing_forward_pass(state: &mut VoicingState, phone: phoneset::Phone)
+                               -> Option<phoneset::Phone> {
+    let original_state = *state;
+    if modifies_voicing(&phone) {
+        *state = if envoices(&phone) { VoicingState::Voice } else { VoicingState::Devoice }
+    }
+    let new_phone = match phone {
+        V => if original_state == VoicingState::Devoice { F } else { V },
+        ph => ph
+    };
+    return Some(new_phone)
 }
 
 pub fn transcribe(word: &str) -> Option<Vec<phoneset::Phone>> {
@@ -185,6 +214,7 @@ pub fn transcribe(word: &str) -> Option<Vec<phoneset::Phone>> {
         }
 
     }
+    word_phones = word_phones.into_iter().scan(VoicingState::Neutral, change_voicing_forward_pass).collect();
     return Some(word_phones);
 }
 
@@ -223,15 +253,15 @@ fn test_letter_c() {
         "pac" => [P A C],
         "paca" => [P A C A],
         "tancbuda" => [T A N Dz B U D A],
-        
+
         "ciasto" => [Ci A S T O],
         "koci" => [K O Ci I],
         "kocia" => [K O Ci A],
-        
+
         "czy" => [Cz Y],
         "bacz" => [B A Cz],
         "baczże" => [B A Dzh Zh E],
-        
+
         "bach" => [B A H],
         "cham" => [H A M]
     );
@@ -252,18 +282,18 @@ fn test_letter_d() {
         "dama" => [D A M A],
         "trud" => [T R U D],
         "kładka" => [K W A T K A],
-        
+
         "widz" => [V I Dz],
         "dzwon" => [Dz V O N],
         "łódzki" => [W U C K I],
-        
+
         "dziad" => [Dzi A D],
         "młodzi" => [M W O Dzi I],
-        
+
         "łódź" => [W U Dzi],
         "dźwig" => [Dzi V I G],
         "pójdźka" => [P U J Ci K A],
-        
+
         "dżem" => [Dzh E M],
         "dżdżownica" => [Dzh Dzh O V Ni I C A],
         "różdżka" => [R U Sz Cz K A]
@@ -298,7 +328,7 @@ fn test_letter_g() {
         "dog" => [D O G],
         "angst" => [A Ng K S T],
         "gwiazda" => [G V J A Z D A]
-            
+
     );
 }
 
@@ -382,6 +412,14 @@ fn test_letter_n() {
 }
 
 #[test]
+fn test_letter_ni() {
+    check_word_transcriptions!(
+        "koń" => [K O Ni],
+        "kończ" => [K O Ni Cz]
+    );
+}
+
+#[test]
 fn test_letter_o() {
     check_word_transcriptions!(
         "polo" => [P O L O],
@@ -390,7 +428,141 @@ fn test_letter_o() {
 }
 
 #[test]
+fn test_letter_oo() {
+    check_word_transcriptions!(
+        "pół" => [P U W],
+        "ósmy" => [U S M Y]
+    );
+}
+
+#[test]
+fn test_letter_p() {
+    check_word_transcriptions!(
+        "pada" => [P A D A],
+        "łup" => [W U P],
+        "krepdeszyn" => [K R E B D E Sz Y N]
+    );
+}
+
+#[test]
+fn test_letter_r() {
+    check_word_transcriptions!(
+        "rada" => [R A D A],
+        "kar" => [K A R],
+        "arka" => [A R K A],
+        "erg" => [E R G],
+        "rzepa" => [Zh E P A],
+        "perz" => [P E Zh]
+    );
+}
+
+#[test]
+fn test_letter_s() {
+    check_word_transcriptions!(
+        "staw" => [S T A V],
+        "podstawa" => [P O T S T A V A],
+        "bejsbol" => [B E J Z B O L],
+
+        "szum" => [Sz U M],
+        "muszla" => [M U Sz L A],
+        "szlam" => [Sz L A M],
+        "mysz" => [M Y Sz],
+        "fiszbin" => [F I Zh B I N],
+
+        "siedzi" => [Si E Dzi I],
+        "mysi" => [M Y Si I]
+    );
+}
+
+#[test]
+fn test_letter_si() {
+    check_word_transcriptions!(
+        "śpiwór" => [Si P I V U R],
+        "myśl" => [M Y Si L],
+        "skoś" => [S K O Si],
+        "prośba" => [P R O Zi B A]
+    );
+}
+
+#[test]
+fn test_letter_t() {
+    check_word_transcriptions!(
+        "trawa" => [T R A V A],
+        "towot" => [T O V O T],
+        "futbol" => [F U D B O L]
+    );
+}
+
+#[test]
+fn test_letter_u() {
+    check_word_transcriptions!(
+        "ukrop" => [U K R O P],
+        "kotu" => [K O T U]
+    );
+}
+
+#[test]
+fn test_letter_w() {
+    check_word_transcriptions!(
+        "witaj" => [V I T A J],
+        "staw" => [S T A V],
+        "gwizd" => [G V I Z D],
+        "kwiat" => [K F I A T],
+        "świat" => [Si F I A T]
+    );
+}
+
+#[test]
+fn test_letter_y() {
+    check_word_transcriptions!(
+        "myśl" => [M Y Si L],
+        "nowy" => [N O V Y]
+    );
+}
+
+#[test]
+fn test_letter_z() {
+    check_word_transcriptions!(
+        "zęby" => [Z E M B Y],
+        "zza" => [Z Z A],
+        "głaz" => [G W A Z],
+
+        "szum" => [Sz U M],
+        "mysz" => [M Y Sz],
+        "czar" => [Cz A R],
+        "pucz" => [P U Cz],
+        "dzyń" => [Dz Y Ni],
+        "rydz" => [R Y Dz],
+        "gdzie" => [G Dzi E],
+        "dzik" => [Dzi I K]
+    );
+}
+
+#[test]
+fn test_letter_zi() {
+    check_word_transcriptions!(
+        "maź" => [M A Zi],
+        "łódź" => [W U Dzi],
+        "weźcie" => [V E Si Ci E],
+        "dźwig" => [Dzi V I G],
+        "idźcie" => [I Ci Ci E]
+    );
+}
+
+#[test]
+fn test_letter_zz() {
+    check_word_transcriptions!(
+        "żaba" => [Zh A B A],
+        "angaż" => [A Ng G A Zh],
+        "móżdżek" => [M U Zh Dzh E K],
+        "dżem" => [Dzh E M],
+        "móżdżku" => [M U Sz Cz K U]
+    );
+}
+
+#[test]
 fn test_failed_transcriptions() {
     assert_eq!(transcribe("beyoncé"), None);
     assert_eq!(transcribe("übermensch"), None);
+    assert_eq!(transcribe("quasi"), None);
 }
